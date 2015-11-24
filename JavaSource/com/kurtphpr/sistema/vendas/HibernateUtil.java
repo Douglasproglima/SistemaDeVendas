baf8fada51a40964f801c2ec5b1a16c8775e7dc7
Douglasproglima <douglasproglima@gmail.com>
2015-11-23 22:24:10 -0200

    Aula07

---
+++ JavaSource/com/kurtphpr/sistema/vendas/HibernateUtil.java
@@ -0,0 +1,22 @@
+package com.kurtphpr.sistema.vendas;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+	private static final SessionFactory session = buildSessionFactory();
+	
+	private static SessionFactory buildSessionFactory() {
+		try {
+			Configuration cfg = new Configuration();
+			cfg.configure("hibernate.cfg.xml");
+			return cfg.buildSessionFactory();
+		} catch (Throwable erro) {
+			System.out.println("Erro na Conexão./n" + erro);
+			throw new ExceptionInInitializerError();
+		}
+	}
+	
+	public static SessionFactory getSession(){
+		return session;
+	}
+}
