1fce1fa66b47e05f19ae18c86c173d7c6075a3ef
Douglasproglima <douglasproglima@gmail.com>
2015-11-23 22:24:10 -0200

    Aula07

---
+++ JavaSource/Conecta.java
@@ -0,0 +1,16 @@
+import org.hibernate.Session;
+import com.kurtphpr.sistema.vendas.HibernateUtil;
+
+public class Conecta {
+	public static void main(String[] args) {
+		Session sessao = null;
+		
+		try {
+			sessao = HibernateUtil.getSession().openSession();
+			System.out.println("Conexão realizada com sucesso.");
+		} finally{
+			sessao.close();
+			System.out.println("Conexão encerrada.");
+		}
+	}
+}
